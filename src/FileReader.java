adbf330fe51d47132a7136b2e4028929905c66e3
Kevin MC <kevinmaldonadocota@gmail.com>
2022-01-29 17:42:58 -0800

    Made some serious changes! Filereader can now get a score and sort the existing file, save its contents into a hashmap then sorts it and then writes it back into the file. Only problem now is that there is a small bug.

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,44 +1,18 @@
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
-import java.util.Vector;
+import java.util.Set;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.*;
 
 public class FileReader {
-	//Followiung code is from geeksforgeeks
-	//Link https://www.geeksforgeeks.org/sorting-a-hashmap-according-to-values/
-	 public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm){
-		 
-	     // Create a list from elements of HashMap\
-		 List<Map.Entry<String, Integer> > list = new LinkedList<Map.Entry<String, Integer> >(hm.entrySet());
-		 
-		 // Sort the list
-		Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
-		    public int compare(Map.Entry<String, Integer> o1,
-		                       Map.Entry<String, Integer> o2)
-		    {
-		        return (o1.getValue()).compareTo(o2.getValue());
-		    }
-		});
-		 
-		// put data from sorted list to hashmap
-		HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
-		for (Map.Entry<String, Integer> aa : list) {
-		    temp.put(aa.getKey(), aa.getValue());
-		}
-			return temp;
-	    }
-
-	public static void main(String[] args){
-		//Vector<Map<String, Integer>> info = new Vector<Map<String, Integer>>();
+	//Function grabs info from
+	//The function will return the sorted hashmap based on values
+	public static HashMap<String, Integer> grabInfoFromFile(){
 		HashMap<String, Integer> dataVal = new HashMap<String, Integer>();
 		try {
 			File leaderboard = new File("C:\\Users\\maldo\\git\\StarShift\\Media\\leaderboard.txt");
@@ -46,12 +20,17 @@ public class FileReader {
 			while(myReader.hasNextLine()) {
 				String data = myReader.nextLine();
 				String[] returnedArr1 = data.split(" ", 4);
+				int num = 0;
 				
 				for(int i = 0; i < returnedArr1.length - 1; i++) {
-					int num = Integer.parseInt(returnedArr1[i + 1]);
+					try {
+						num = Integer.parseInt(returnedArr1[i + 1]);
+					}
+					catch(NumberFormatException e) {
+						returnedArr1[i + 1].replaceAll(" ", "");
+					}
 					dataVal.put(returnedArr1[i], num);
-
-				}
+				}	
 			}
 			myReader.close();
 		}
@@ -59,13 +38,94 @@ public class FileReader {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
-		HashMap<String, Integer> sort = sortByValue(dataVal);
-		//Sort the hashMap
-		
-		for(Entry<String, Integer> entry : sort.entrySet()) {
-			System.out.println("Name: " + entry.getKey() + ", Score: " + entry.getValue());
+		HashMap<String, Integer> sort = SortHash.sortByValue(dataVal);
+		return sort;
+	}
+	public static int getMin(HashMap<String, Integer> data) {
+		boolean firstIteration = true;
+		int temp = 0;
+		for(Entry<String, Integer> entry2 : data.entrySet()) {
+			if(firstIteration) {
+				temp = entry2.getValue();
+				firstIteration = false;
+			}
+			else if(entry2.getValue() < temp) {
+				temp = entry2.getValue();
+			}
 		}
+		return temp;
+	}
+	public static HashMap<String, Integer> inputNewScore(int score, HashMap<String, Integer> data){
+		String userName;
+		Scanner myObj = new Scanner(System.in);
+		int min = 0;
+		if(data.size() < 10) {
+			System.out.print("Enter your Name: ");
+			userName = myObj.nextLine();
+			data.put(userName, score);
+		}
+		else {
+			for(Entry<String, Integer> entry : data.entrySet()) {
+				if(score > entry.getValue()) {
+					System.out.print("Enter your Name: ");
+					userName = myObj.nextLine();		
+					data.put(userName, score);
+					min = getMin(data);				
+				}
+			}	
+		}
+		Set<Entry<String, Integer>> entries = data.entrySet();
+		Iterator<Entry<String, Integer>> iterator = entries.iterator();
+		while(iterator.hasNext()) {
+			Entry<String, Integer> entry = iterator.next();
+			int val = entry.getValue();
+			if(val == min) {
+				iterator.remove();
+			}
+		}
+		data = SortHash.sortByValue(data);
+		myObj.close();
+		File leaderboard = new File("C:\\Users\\maldo\\git\\StarShift\\Media\\leaderboard.txt");
+		leaderboard.delete();
+		try {
+			leaderboard.createNewFile();
+		} catch (IOException e1) {
+			System.out.println("An error occurred.");
+			e1.printStackTrace();
+		}
+		try {
+			FileOutputStream editedLeaderboard = new FileOutputStream(leaderboard, true);
+			byte[] bytes;
+			for(Entry<String, Integer> entry : data.entrySet()) {
+				bytes = (entry.getKey() + " " + entry.getValue() + "\n" ).getBytes();
+				try {
+					editedLeaderboard.write(bytes);
+				} catch (IOException e) {
+					System.out.println("An error occurred.");
+					e.printStackTrace();
+				}
+				
+			}
+			try {
+				editedLeaderboard.close();
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
 		
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		return data;
+	}
+	
+
+	public static void main(String[] args){
+		HashMap<String, Integer> rando = new HashMap<String, Integer>();
+		rando = grabInfoFromFile();
+		rando = inputNewScore(9999, rando);
+
 	}
 		
 }
